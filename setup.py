661674954cf354b414af46ae2496ee429321e3d1
MBoulahtouf <146747394+MBoulahtouf@users.noreply.github.com>
2023-11-14 00:01:40 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In an age where digital information is ubiquitous, distinguishing between genuine and misleading news is more crucial than ever. Our Fake News Detection project addresses this contemporary challenge using the tools and techniques of Natural Language Processing (NLP). The primary objective is to construct a machine learning model adept at discerning the alignment of a news headline with its corresponding article body. This endeavor is not just an academic exercise but a vital step towards mitigating the spread of misinformation in today's fast-paced digital world.',
+    author='Nourhene Hamdene & Marouan Boulahtouf',
+    license='MIT',
+)
